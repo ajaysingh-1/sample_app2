731fe584f1f5a0bcc82801fa458cdc8d34c16556
ajay <ajshekhawat62@gmail.com>
2022-01-16 12:39:29 +0530

    Finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_15_102943) do
+ActiveRecord::Schema.define(version: 2022_01_16_052959) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2022_01_15_102943) do
     t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
